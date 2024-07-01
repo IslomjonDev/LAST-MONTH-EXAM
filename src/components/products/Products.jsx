757abc743c3e85354951d2725651b705c30794f9
IslomjonDev <mamadiyevislomjon2@gmail.com>
2024-07-01 12:01:23 +0500

    single Route

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,54 +3,47 @@ import { useGetCategorysQuery, useGetProductByIdQuery, useGetProductsQuery } fro
 import './Products.scss'
 import { FaArrowRightLong } from "react-icons/fa6";
 import { IoCartOutline , IoHeartOutline } from "react-icons/io5";
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import Modul from '../modul/Modul';
 
-const Products = () => {
-  
+const Products = ({ sliceCount = 8 }) => {
   const [detail, setDetail] = useState(null)
   const { data } = useGetProductsQuery()
   const { data : categorys } = useGetCategorysQuery()
   const [searchParams, setSearchParams] = useSearchParams()
-  
-  
-  
-  
-  
+
   let id = searchParams.get('detail')
   const { data: databyId } = useGetProductByIdQuery(id)
-  
+
   useEffect(() => {
     if (id) {
       setDetail(databyId);
     }
-  }, [searchParams])
-  
-  
+  }, [searchParams, databyId])
+
   const closeDetail = () => {
     setDetail(null)
     setSearchParams({})
   }
-  
-
-
 
   let products__category = categorys?.map(category => (
     <button className='category__btn' key={category.id}>{category.category}</button>
   ))
 
-  let products = data?.slice(0, 8).map(product => (
+  let products = data?.slice(0, sliceCount).map(product => (
     <div className="products__item" key={product.id}>
       <div className="imgp">
         <div className='imgp2' onClick={() => setSearchParams({ detail: product?.id })}>
-          <img  src={product?.image} alt={product?.image} />
+          <img src={product?.image} alt={product?.image} />
         </div>
         <button>
           <IoHeartOutline />
         </button>
       </div>
       <div className="pro__prag">
-        <h3>{product.title}</h3>
+        <Link to={`/products/${product.id}`}>
+         <h3>{product.title}</h3>
+        </Link>
         <div className="price">
           <span>
             <p>{product.oldPrice}</p>
@@ -69,8 +62,10 @@ const Products = () => {
         <div className="container">
           <div className="products__title">
             <h2>Популярные товары</h2>
-            <button>Все товары <FaArrowRightLong />
-            </button>
+            <Link to={'/all-products'}>
+              <button>Все товары <FaArrowRightLong />
+              </button>
+            </Link>
           </div>
           <div className="product__categorys">
             {products__category}
@@ -78,16 +73,18 @@ const Products = () => {
           <div className="product__cards">
             {products}
           </div>
-          <div className="product__btn">
-            <button>Все товары <FaArrowRightLong /></button>
+          <div  className="product__btn">
+            <Link to={'/all-products'}>
+               <button>Все товары <FaArrowRightLong /></button>
+            </Link>
           </div>
         </div>
         {
           detail ? 
           <Modul close={closeDetail}>
               <div className='detail'>
-                        <img src={detail?.image} alt="" />
-                  </div>
+                <img src={detail?.image} alt="" />
+              </div>
           </Modul>
           :
           <></>
