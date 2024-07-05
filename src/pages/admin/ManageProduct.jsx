e108e3dc8684ec05182da39c962673e29102928b
IslomjonDev <mamadiyevislomjon2@gmail.com>
2024-07-05 23:00:08 +0500

    udatezt

--- src/pages/admin/ManageProduct.jsx
+++ src/pages/admin/ManageProduct.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Products from '../../components/products/Products'
-import { useUpdateProductMutation } from '../../context/api/productApi'
+import { useGetProductsQuery, useUpdateProductMutation } from '../../context/api/productApi'
 
-const ManageProduct = ({data}) => {
+const ManageProduct = () => {
 
-  let {data} = useUpdateProductMutation()
+ let {data}  = useGetProductsQuery()
 
   return (
     <>
-      
+      <h2>Manage Product</h2>
+      <Products data={data}/>
     </>
   )
 }
